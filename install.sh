ed00d693a597343c9f0e60a200007653f3e7b4f0
Bryce Larson <brycelarsn@gmail.com>
2017-03-01 23:21:11 -0700

    Made install script and github-shortcuts better

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo -e "\e[33mInput your github username for github-shortcuts:\e[39m"
+read line
+sed -i "s/yourgithubusernamehere/$line/" github-shortcuts
+
+echo -e "\e[33mMaking ~/.bash directory\e[39m"
 mkdir $HOME/.bash
-cp git* $HOME/.bash/
+
+echo -e "\e[33mCopying scripts to ~/.bash/\e[39m"
+cp -ai git* $HOME/.bash/
+
+echo -e "\e[33mAppending bashrc to ~/.bashrc\e[39m"
 cat bashrc >> $HOME/.bashrc
