f2c6099eac4552b08fe0901e9353b9e619be4729
Bryce Larson <brycelarsn@gmail.com>
2017-11-28 12:29:02 -0700

    added aliases to install script

--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ cp -ai git* $HOME/.bash/
 
 echo -e "\e[33mAppending bashrc to ~/.bashrc\e[39m"
 cat bashrc >> $HOME/.bashrc
+
+echo -e "\e[33mAdding aliases to git\e[39m"
+git config --global alias.graph "log --graph --pretty=format:'%C(auto)%d %h %cr %Cblue%an %Creset %s' --branches"
+git config --global alias.ls "ls-files"
+git config --global alias.count "rev-list HEAD --count"
+git config --global alias.wdiff "diff --word-diff"
