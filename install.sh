394244923ec7155eafdd99ae2b9602b9eb800c0b
Bryce Larson <blarson@saltstack.com>
2019-06-08 11:32:30 -0600

    remove --branches so I can deal with more complex environments

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ else
 fi
 
 echo -e "\e[33mAdding aliases to ~/.gitconfig\e[39m"
-git config --global alias.graph "log --graph --pretty=format:'%C(auto)%d %h %cr %Cblue%an %Creset %s' --branches"
+git config --global alias.graph "log --graph --pretty=format:'%C(auto)%d %h %cr %Cblue%an %Creset %s'"
 git config --global alias.ls "ls-files"
 git config --global alias.count "rev-list HEAD --count"
 git config --global alias.wdiff "diff --word-diff"
